589ce520be8176aea002a1deb94466b3639d7e96
Yaozhong Li <yaozhong@moov.cc>
2017-12-02 17:24:04 +0800

    use attrs

--- app/src/main/java/code/joshuali/readmoreview/MainActivity.kt
+++ app/src/main/java/code/joshuali/readmoreview/MainActivity.kt
@@ -2,12 +2,17 @@ package code.joshuali.readmoreview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<ReadMoreView>(R.id.readmore).setText("Thank you for using GitHub! We're happy you're here. Please read this Terms of Service agreement carefully before accessing or using GitHub. Because it is such an important contract between us and our users, we have tried to make it as clear as possible. For your convenience, we have presented these terms in a short non-binding summary followed by the full legal terms.")
     }
+
+    fun onClick(view: View) {
+        findViewById<ReadMoreView>(R.id.readmore).setText("Thank you for using GitHub! ")
+    }
+
 }
--- app/src/main/java/code/joshuali/readmoreview/ReadMoreView.kt
+++ app/src/main/java/code/joshuali/readmoreview/ReadMoreView.kt
@@ -7,7 +7,7 @@ import android.text.Layout
 import android.text.StaticLayout
 import android.text.TextPaint
 import android.util.AttributeSet
-import android.util.DisplayMetrics
+import android.util.Log
 import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
@@ -18,13 +18,17 @@ import android.view.View
 class ReadMoreView : View{
 
 
-    var descriptionText: String = ""
-
-    private val truncateLines: Int = 4
+    private var text: String = ""
+    private var moreText:String = "more"
+    private var textColor = Color.BLACK
+    private var moreTextColor = Color.RED
+    private var textSize = 30f
+    private var faddingLength = 40f
+    private var faddingPadding = 8f
+    private var truncateLines: Int = Int.MAX_VALUE
 
     private var staticLayout : StaticLayout? = null
     private var moreStaticLayout : StaticLayout? = null
-
     private var textPaint = TextPaint()
     private var moreTextPaint = TextPaint()
     private var moreBgPaint = Paint()
@@ -33,44 +37,60 @@ class ReadMoreView : View{
     private var singleLineHeight = 0
     private var moreWidth = 0f
     private var moreHeight = 0
-
     private var showAll = false
 
     init {
         isEnabled = true
         textPaint.isAntiAlias = true
-        textPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 14f, resources.displayMetrics)
-        textPaint.color = Color.BLACK
+        textPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, textSize, resources.displayMetrics)
+        textPaint.color = textColor
 
         moreTextPaint.isAntiAlias = true
-        moreTextPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 14f, resources.displayMetrics)
-        moreTextPaint.color = Color.RED
+        moreTextPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, textSize, resources.displayMetrics)
+        moreTextPaint.color = moreTextColor
 
     }
 
-    constructor(ctx: Context) : super(ctx) {
-    }
+    constructor(ctx: Context) : super(ctx)
     constructor(ctx: Context, attrs: AttributeSet) : super(ctx,attrs) {
+        getAttr(ctx, attrs)
+    }
+
+    private fun getAttr(ctx: Context, attrs: AttributeSet) {
+        val typedArray = ctx.obtainStyledAttributes(attrs, R.styleable.ReadMoreViewAttr)
+        text = typedArray.getString(R.styleable.ReadMoreViewAttr_text) ?: ""
+        moreText = typedArray.getString(R.styleable.ReadMoreViewAttr_moreText) ?: moreText
+        textColor = typedArray.getColor(R.styleable.ReadMoreViewAttr_textColor, textColor)
+        moreTextColor = typedArray.getColor(R.styleable.ReadMoreViewAttr_moreTextColor, moreTextColor)
+        textSize = typedArray.getDimension(R.styleable.ReadMoreViewAttr_textSize, textSize)
+        faddingLength = typedArray.getDimension(R.styleable.ReadMoreViewAttr_faddingLength, 0f)
+        faddingPadding = typedArray.getDimension(R.styleable.ReadMoreViewAttr_faddingPadding, 0f)
+        truncateLines = typedArray.getInt(R.styleable.ReadMoreViewAttr_showLine, Int.MAX_VALUE)
+        typedArray.recycle()
+
+        textPaint.color = textColor
+        textPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, textSize, resources.displayMetrics)
+        moreTextPaint.color = moreTextColor
+        moreTextPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, textSize, resources.displayMetrics)
     }
 
     fun setText(text: String) {
-        descriptionText = text
-        invalidate()
+//        requestLayout()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         val width = measuredWidth
-        if (width > 0 && descriptionText.isNotEmpty() && staticLayout == null) {
-            moreStaticLayout = StaticLayout("more", moreTextPaint, width, Layout.Alignment.ALIGN_NORMAL, 1f, 0f, false)
+        if (width > 0 && staticLayout == null) {
+            moreStaticLayout = StaticLayout(moreText, moreTextPaint, width, Layout.Alignment.ALIGN_NORMAL, 1f, 0f, false)
             singleLineHeight = StaticLayout("", textPaint, width, Layout.Alignment.ALIGN_NORMAL, 1f, 0f ,false).height
             moreWidth = moreStaticLayout!!.getLineWidth(0)
             moreHeight = moreStaticLayout!!.height
-            val totalWidth = moreWidth + dpToPx(40)
+            val totalWidth = moreWidth + faddingLength + faddingPadding
 
-            gradient = LinearGradient(0f, 0f, totalWidth, moreHeight.toFloat(), intArrayOf(Color.parseColor("#00FFFFFF"), Color.parseColor("#FFFFFFFF"), Color.parseColor("#FFFFFFFF")), floatArrayOf(0f, (dpToPx(32)/totalWidth), 1f), Shader.TileMode.CLAMP)
+            gradient = LinearGradient(0f, 0f, totalWidth, moreHeight.toFloat(), intArrayOf(Color.parseColor("#00FFFFFF"), Color.parseColor("#FFFFFFFF"), Color.parseColor("#FFFFFFFF")), floatArrayOf(0f, faddingLength/totalWidth, 1f), Shader.TileMode.CLAMP)
             moreBgPaint.shader = gradient
-            staticLayout = StaticLayout(descriptionText,0, descriptionText.length, textPaint, width, Layout.Alignment.ALIGN_NORMAL, 1f, 0f ,false)
+            staticLayout = StaticLayout(text,0, text.length, textPaint, width, Layout.Alignment.ALIGN_NORMAL, 1f, 0f ,false)
             showAll = staticLayout!!.height <= singleLineHeight * truncateLines
         }
         setMeasuredDimension(width, if (showAll) staticLayout!!.height else Math.min(staticLayout!!.height, singleLineHeight * truncateLines))
@@ -79,7 +99,7 @@ class ReadMoreView : View{
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         if (event != null && !showAll) {
-            if (event.x >= moreWidth && event.y >= moreHeight) {
+            if (event.x >= width - moreWidth && event.y >= height - moreHeight) {
                 showAll = true
                 requestLayout()
             }
@@ -91,19 +111,13 @@ class ReadMoreView : View{
         staticLayout?.draw(canvas)
         if (!showAll) {
             canvas?.save()
-            canvas?.translate(width - moreWidth - dpToPx(40), (height - moreHeight).toFloat())
-            canvas?.drawRect(0f, 0f, moreWidth + dpToPx(40), moreHeight.toFloat(), moreBgPaint)
-            canvas?.translate(dpToPx(40).toFloat(), 0f)
+            canvas?.translate(width - moreWidth - faddingLength - faddingPadding, (height - moreHeight).toFloat())
+            canvas?.drawRect(0f, 0f, moreWidth + faddingLength + faddingPadding, moreHeight.toFloat(), moreBgPaint)
+            canvas?.translate(faddingLength + faddingPadding, 0f)
             moreStaticLayout?.draw(canvas)
             canvas?.restore()
         }
         super.onDraw(canvas)
-    }
-
-    private fun dpToPx(dp: Int): Int {
-        val resources = context.resources
-        val metrics = resources.displayMetrics
-        return (dp * (metrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)).toInt()
     }
 
 }
